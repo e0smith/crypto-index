e0cc720c57709fa1721fe7a9d8f6801836e2a4bc
Edward Smith Silvia <esmithsilvia@gmail.com>
2021-02-22 21:06:14 -0500

    enviorment and gemfile modifications

--- lib/api.rb
+++ lib/api.rb
@@ -1,3 +1,14 @@
+#stores data from api
+
 class POKE::API
 
+    def initialize
+        @url = "https://pokeapi.co/api/v2/pokemon?limit=151"
+    end
+
+
+    def get_pokemon
+        poke_hash = HTTParty.get(@url)
+        binding.pry
+    end
 end
--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,4 +1,3 @@
 class POKE::Pokemon
 
-
 end
