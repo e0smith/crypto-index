766a05b5b56a40014fb1361856e7e184295c739f
Edward Smith Silvia <esmithsilvia@gmail.com>
2021-02-22 22:10:02 -0500

    CLI prompts

--- config/enviorment.rb
+++ config/enviorment.rb
@@ -3,7 +3,9 @@ Bundler.require
 require 'pry'
 require 'httparty'
 
+
 require_relative '../lib/modules/poke.rb'
 require_relative '../lib/pokemon.rb'
 require_relative '../lib/api.rb'
 require_relative '../lib/cli.rb'
+
--- lib/cli.rb
+++ lib/cli.rb
@@ -1,3 +1,27 @@
 class POKE::CLI
+    #inputs go here
+    def run
+        loading
+        greeting
+    end
 
+    def loading
+        puts "----------------------------------------------------"
+        puts "---------------Loading up the PokeDex---------------"
+        puts "----------------------------------------------------"
+        sleep(2)
+        num = 0
+        while num <= 100
+            puts "PROGRESS #{num}%"
+            num +=1
+            sleep(1.0/48.0)
+        end
+    end
+
+    def greeting
+        puts "Welcome to the Pokemon API"
+        puts "Please press enter to see your selection of pokemon..."
+    end
+    
 end
+ 
